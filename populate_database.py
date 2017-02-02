7453f7d4f8468cb86526267104927b378f97d963
Flavio D Johansson <flaviodaniel@outlook.com>
2017-02-01 22:23:15 -0200

    Add: populate categories table

---
+++ populate_database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Category
+
+engine = create_engine('sqlite:///thehandicrafter.db')
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+session.add(Category(name='Woodwork'))
+session.add(Category(name='Knitting'))
+session.add(Category(name='Sewing'))
+session.add(Category(name='Papercraft'))
+session.add(Category(name='Decoupage'))
+session.commit()
+
+
+print 'added categories !'
