7c1e0cc2dc6e3934ddacfa37ba0e95a45a8d6454
Flavio D Johansson <flaviodaniel@outlook.com>
2017-02-27 13:46:12 -0300

    Add simple yet efficient CSRF ontrol

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, session
 from flask_debugtoolbar import DebugToolbarExtension
 from os import path
 from jinja2 import Markup
+import random, string
 
 # Blueprint
 from app.controllers.home import home
@@ -37,3 +38,26 @@ def not_found(error):
 def nl2br(value):
     # Markup makes the value safe as in the filter safe
     return Markup(value.replace('\n', '<br>'))
+
+
+# CSRF Protection
+# As seen at http://flask.pocoo.org/snippets/3/
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+
+def generate_csrf_token():
+    # Should I use openssl_random_pseudo_bytes ??
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for x in xrange(64)
+        )
+    return session['_csrf_token']
+
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
